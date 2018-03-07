b5985b182fa0014d9316d589444bc000c3bfd0a4
edgararuiz <edgararuiz@gmail.com>
2018-03-06 19:01:16 -0600

    Adds multi-reporter

--- R/testthat.R
+++ R/testthat.R
@@ -1,17 +1,26 @@
 #' @export
-test_database <- function(tests = "default"){
-  with_reporter(default_reporter(), testthat_database(tests))
+test_database <- function(tests = "default") {
+  reporter <- MultiReporter$new(
+    reporters = list(ProgressReporter$new(), ListReporter$new())
+  )
+  r <- with_reporter(reporter, testthat_database("default"))
+
+  df <- structure(
+    r$reporters[[2]]$results$as_list(),
+    class = "testthat_results"
+  )
+  df <- as.data.frame(df)
+  df[, 2:length(df)]
 }
 
-testthat_database <- function(tests = "default"){
-
-  if(tests == "default"){
+testthat_database <- function(tests = "default") {
+  if (tests == "default") {
     tests <- read_yaml(default_tests_path())
   } else {
-    if(class(tests) == "character") tests <- read_yaml(tests)
+    if (class(tests) == "character") tests <- read_yaml(tests)
   }
 
-  if(class(tests) != "list") error("Tests need to be in YAML format")
+  if (class(tests) != "list") error("Tests need to be in YAML format")
 
   local_df <- mtcars %>%
     mutate(
@@ -25,14 +34,14 @@ testthat_database <- function(tests = "default"){
       y = wt
     )
 
-  run_test <- function(verb, vector_expression){
+  run_test <- function(verb, vector_expression) {
     f <- parse_expr(vector_expression)
 
-    if(verb == "summarise") manip <- . %>% summarise(!!f) %>% pull()
-    if(verb == "mutate")    manip <- . %>% mutate(!!f)    %>% pull()
-    if(verb == "arrange")   manip <- . %>% arrange(!!f)   %>% pull()
-    if(verb == "filter")    manip <- . %>% filter(!!f)    %>% pull()
-    if(verb == "group_by")  manip <- . %>% group_by(!!f)  %>% summarise() %>% pull()
+    if (verb == "summarise") manip <- . %>% summarise(!! f) %>% pull()
+    if (verb == "mutate") manip <- . %>% mutate(!! f) %>% pull()
+    if (verb == "arrange") manip <- . %>% arrange(!! f) %>% pull()
+    if (verb == "filter") manip <- . %>% filter(!! f) %>% pull()
+    if (verb == "group_by") manip <- . %>% group_by(!! f) %>% summarise() %>% pull()
 
     test_that(paste0(vector_expression), {
       invisible({
@@ -62,6 +71,4 @@ testthat_database <- function(tests = "default"){
         flatten() %>%
         map(~run_test(verb, .x))
     })
-
 }
-
