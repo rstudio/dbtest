cfb62637b014eeb47b8e77136ce2c21ef1fb3691
edgararuiz <edgararuiz@gmail.com>
2018-03-08 12:19:55 -0600

    Improves imports

--- R/dbtest.R
+++ R/dbtest.R
@@ -5,8 +5,13 @@
 #' @importFrom testthat context
 #' @importFrom testthat expect_equal
 #' @importFrom testthat test_that
+#' @importFrom testthat MultiReporter
+#' @importFrom testthat MinimalReporter
+#' @importFrom testthat ListReporter
 #' @import htmltools
 #' @import ggplot2
+#' @import DBI
+#' @import rlang
 #' @import utils
 #' @import pracma
 #' @import dplyr
