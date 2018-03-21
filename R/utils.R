876adcbacfc2d5065cad9ed13f071ac0299a3588
Cole Arendt <cole@rstudio.com>
2018-03-21 13:08:16 +0000

    rename default_test_path update all_tests

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,24 @@
 #' @rdname utils
 #' @title Utility Functions
 #'
-#' @description Utility functions useful for package default behavior
+#' @description Utility functions useful for package default behavior.
+#' `default_test_path` returns the path to tests from the `dbtest` package.
+#' `default_config_path` returns the path to connection config from the `dbtest`
+#' package.  `all_tests` returns the path to all .yml or .yaml files in a given
+#' directory.  The default is to return all tests in the `dbtest` package
 #'
-#' @param file The file to reference at the default location
+#' @param file optional The file to reference at the default location
+#' @param dir optional The directory for which to show all_tests
 #'
 #' @export
-default_tests_path <- function(file = "simple-tests.yml"){
+default_test_path <- function(file = "simple-tests.yml"){
   system.file("extdata", "tests", fs::path_file(file), package = "dbtest")
 }
 
+#' @rdname utils
+#' @export
 all_tests <- function(dir = system.file("extdata", "tests", package = "dbtest")) {
-  fs::dir_ls(dir)
+  fs::dir_ls(dir) %>% purrr::keep(tolower(fs::path_ext(.)) %in% c("yml","yaml"))
 }
 
 #' @rdname utils
