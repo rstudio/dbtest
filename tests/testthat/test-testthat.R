cfc64d9831bc99d28003cd6c1509c536706fde87
Cole Arendt <cole@rstudio.com>
2018-03-22 20:00:23 +0000

    add package tests

---
+++ tests/testthat/test-testthat.R
@@ -0,0 +1,49 @@
+context("test_single_database")
+
+test_that("works with a connection object", {
+  con <- dbConnect(RSQLite::SQLite(),":memory:")
+  output <- test_single_database(con
+                                 , pkg_test("simple-tests.yml"))
+  dbDisconnect(con)
+
+  expect_s3_class(output$results, "testthat_results")
+  expect_equal(length(output),2)
+})
+
+test_that("works with tbl_sql object", {
+  con <- dbConnect(RSQLite::SQLite(), ":memory:")
+  tdat <- copy_to(con, testdata, "test-single-database")
+  output <- test_single_database(tdat
+                                 , pkg_test("simple-tests.yml"))
+  dbDisconnect(con)
+
+  expect_s3_class(output$results, "testthat_results")
+  expect_equal(length(output),2)
+})
+
+context("test_databases")
+
+test_that("works with a yaml file", {
+  con <- dbConnect(RSQLite::SQLite(),":memory:")
+  output <- test_databases(pkg_config("config.yml")
+                           , pkg_test("simple-tests.yml"))
+  dbDisconnect(con)
+
+  expect_s3_class(output[[1]]$results, "testthat_results")
+  expect_equal(length(output[[1]]),2)
+})
+
+test_that("works with multiple connections", {
+  con <- dbConnect(RSQLite::SQLite(),":memory:")
+  output <- test_databases(pkg_config("multiple.yml")
+                           , pkg_test("simple-tests.yml"))
+  dbDisconnect(con)
+
+  expect_s3_class(output[[1]]$results, "testthat_results")
+  expect_s3_class(output[[2]]$results, "testthat_results")
+  expect_equal(length(output),2)
+  expect_equal(
+    lapply(output, length) %>% as.double()
+    , c(2,2)
+  )
+})
