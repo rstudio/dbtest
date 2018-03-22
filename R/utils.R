cab2267b69a8326051e223d4a639ab49429fee10
Cole Arendt <cole@rstudio.com>
2018-03-22 03:23:32 +0000

    update utilities

--- R/utils.R
+++ R/utils.R
@@ -12,17 +12,52 @@
 #'
 #' @export
 default_test_path <- function(file = "simple-tests.yml"){
-  system.file("extdata", "tests", fs::path_file(file), package = "dbtest")
+  system.file("extdata", "tests", path_file(file), package = "dbtest")
 }
 
 #' @rdname utils
 #' @export
 all_tests <- function(dir = system.file("extdata", "tests", package = "dbtest")) {
-  fs::dir_ls(dir) %>% purrr::keep(tolower(fs::path_ext(.)) %in% c("yml","yaml"))
+  dir_ls(dir) %>% purrr::keep(tolower(path_ext(.)) %in% c("yml","yaml"))
 }
 
 #' @rdname utils
 #' @export
 default_config_path <- function(file = "config.yml"){
   system.file("extdata", "connections", file, package = "dbtest")
+}
+
+#' @title Write Tests
+#'
+#' @description A utility to make writing tests a bit easier
+#' and more reproducible
+#'
+#' @param file The file the tests should be written to
+#' @param header The header to name the test section.  Preferably short
+#' @param expression The expression to use for writing tests
+#'
+#' @export
+write_test <- function(file, header, expression, overwrite = FALSE, comparison = .x > sample(1:10,1)) {
+  existing <- if (fs::file_exists(file) && !overwrite) read_yaml(file) else list()
+
+  compare <- enquo(comparison)
+  print(compare)
+  # need to figure out rlang semantics
+
+  new <- list(
+    list(
+      header=list(
+        "mutate" = expression
+        , "filter" = expression
+        , "summarize" = paste0("sum(",expression,", na.rm = TRUE)")
+        , "group_by" = expression
+        , "arrange" = expression
+      )
+    )
+  )
+
+  write_yaml(
+    c(existing, new)
+    ,file
+    )
 }
