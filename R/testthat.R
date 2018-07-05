ee1431c6233f6ee555afb47effb973ff151f6bcf
Cole Arendt <cole.arendt@outlook.com>
2018-07-05 16:52:01 -0400

    add test protection to tbl_sql

--- R/testthat.R
+++ R/testthat.R
@@ -178,7 +178,24 @@ test_database.DBIConnection <- function(datasource = NULL, tests = pkg_test(), r
 #' @export
 test_database.tbl_sql <- function(datasource = NULL, tests = pkg_test(), return_list = TRUE) {
   message("TBL_SQL")
-  output <- test_single_database_impl(datasource = datasource, tests = tests, label = datasource[["ops"]][["x"]])
+  test_output <- withRestarts({
+    tryCatch({
+      output <- test_single_database_impl(
+        datasource = datasource
+        , tests = tests
+        , label = datasource[["ops"]][["x"]]
+        )
+      invisible(output)
+    }, error = function(e){
+      message(e);
+      invokeRestart(
+        "fail_tests"
+        , msg = e
+        , tests = tests
+        , label = label
+      )
+    })
+  }, "fail_tests" = force_failed_tests)
 
   if (return_list) {
     return(list(output))
