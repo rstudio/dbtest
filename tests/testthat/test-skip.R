44aa717d2755cfa2961c34e93cba839a28778ec8
Cole Arendt <cole@rstudio.com>
2018-07-12 11:09:58 +0000

    move skip tests

---
+++ tests/testthat/test-skip.R
@@ -0,0 +1,29 @@
+context("skip")
+
+test_that("handles specific test", {
+  skip("TODO: write test")
+})
+
+test_that("wildcard match database", {
+  skip("TODO: write test")
+})
+
+test_that("wildcard match file", {
+  skip("TODO: write test")
+})
+
+test_that("wildcard match context", {
+  skip("TODO: write test")
+})
+
+test_that("wildcard match test", {
+  skip("TODO: write test")
+})
+
+test_that("wildcard match all does nothing", {
+  skip("TODO: write test")
+})
+
+test_that("handles multiple skip files", {
+  skip("TODO: write test")
+})
--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -255,32 +255,3 @@ test_that("recovers from a bad connection state", {
   DBI::dbDisconnect(con)
 })
 
-context("skip data")
-
-test_that("handles specific test", {
-  skip("TODO: write test")
-})
-
-test_that("wildcard match database", {
-  skip("TODO: write test")
-})
-
-test_that("wildcard match file", {
-  skip("TODO: write test")
-})
-
-test_that("wildcard match context", {
-  skip("TODO: write test")
-})
-
-test_that("wildcard match test", {
-  skip("TODO: write test")
-})
-
-test_that("wildcard match all does nothing", {
-  skip("TODO: write test")
-})
-
-test_that("handles multiple skip files", {
-  skip("TODO: write test")
-})
